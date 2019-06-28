ccd4f340623b9b44d23cb38425308f704c21f567
Nicolas <remyblunt_@hotmail.com>
2019-06-28 12:23:43 -0400

    Commit Grid Responsive

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,9 @@
         <b-collapse is-nav id="nav_collapse">
           <b-navbar-nav>
             <b-nav-item :to="{ name : 'home'}" exact>Home</b-nav-item>
-            <b-nav-item :to="{name : 'about'}">About</b-nav-item>
+            <b-nav-item :to="{ name : 'about'}">About</b-nav-item>
             <b-nav-item :to="{name : 'grid'}">Grid</b-nav-item>
+            <b-nav-item :to="{name : 'cards'}">Cards</b-nav-item>
           </b-navbar-nav>
         </b-collapse>
       </b-container>
---
+++ src/views/Grid.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="mt-5">
+    <b-row>
+      <!-- cols para dispositivos pequeños y md para dispositivos medianos -->
+      <b-col cols="12" md="6" lg="4">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Velit nobis tempore corporis porro laborum voluptas recusandae,
+          quo placeat esse ex? Maxime illo ipsam mollitia dolores suscipit
+          vitae eius perferendis vero.
+        </p>
+      </b-col>
+      <b-col cols="12" md="6" lg="8">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Velit nobis tempore corporis porro laborum voluptas recusandae,
+          quo placeat esse ex? Maxime illo ipsam mollitia dolores suscipit
+          vitae eius perferendis vero.
+        </p>
+      </b-col>
+    </b-row>
+    <b-row>
+      <!-- cols para dispositivos pequeños y md para dispositivos medianos -->
+      <b-col cols="12" md="6" lg="4">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Velit nobis tempore corporis porro laborum voluptas recusandae,
+          quo placeat esse ex? Maxime illo ipsam mollitia dolores suscipit
+          vitae eius perferendis vero.
+        </p>
+      </b-col>
+      <b-col cols="12" md="6" lg="8">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Velit nobis tempore corporis porro laborum voluptas recusandae,
+          quo placeat esse ex? Maxime illo ipsam mollitia dolores suscipit
+          vitae eius perferendis vero.
+        </p>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name :  'Grid'
+};
+</script>
